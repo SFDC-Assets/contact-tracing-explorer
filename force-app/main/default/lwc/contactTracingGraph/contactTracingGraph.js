35910de1d5cde43e23482d149ed64bba2dbd6e5e
Mark Lott <markslott@gmail.com>
2020-12-01 14:47:56 -0600

    refactored LWC

--- force-app/main/default/lwc/contactTracingGraph/contactTracingGraph.js
+++ force-app/main/default/lwc/contactTracingGraph/contactTracingGraph.js
@@ -63,223 +63,164 @@ export default class ContactTracingGraph extends LightningElement {
 
     initializeD3() {
 
-        var links = this.data.links.map(d => Object.create(d));
-        var nodes = this.data.nodes.map(d => Object.create(d));
-        const height = 700;
-        const width = 1200;
-        console.log(this.data);
-        console.log(links);
-        console.log(nodes);
-        const simulation = d3.forceSimulation(nodes)
-            .force("link", d3.forceLink(links).id(d => d.id).distance(50))
-            .force("charge", d3.forceManyBody().strength(-400))
-            .force("x", d3.forceX())
-            .force("y", d3.forceY());
-
-        this.svg = d3.select(this.template.querySelector(".d3"))
-            .append('svg')
-            .attr("viewBox", [-width / 2, -height / 2, width, height])
-            .style("font", "12px sans-serif");
-
-        var svg = this.svg;
-
-        var color = d3.scaleOrdinal(types, d3.schemeCategory10);
-        var ticked = () => {
-            link.attr("d", linkArc);
-            node.attr("transform", d => `translate(${d.x},${d.y})`);
-        }
-
-        var update = () => {
-            // Make a shallow copy to protect against mutation, while
-            // recycling old nodes to preserve position and velocity.
-            const old = new Map(node.data().map(d => [d.id, d]));
-            nodes = this.data.nodes.map(d => Object.assign(old.get(d.id) || {}, d));
-            links = this.data.links.map(d => Object.assign({}, d));
-
-            node = node
-                .data(nodes, d => d.id)
-                .join(enter => {
-                    var ret = enter.append("g")
-                        .call(drag(simulation))
-                        .on('mouseover', nodeMouseOver)
-                        .on('mouseout', nodeMouseOut)
-                        .on('click', nodeClicked);
-                    ret.append("circle")
-                        .attr("stroke", "white")
-                        .attr("stroke-width", 1.5)
-                        .attr("r", 4);
-                    ret.append("text")
-                        .attr("x", 8)
-                        .attr("y", "0.31em")
-                        .text(d => d.name)
-                        .clone(true).lower()
-                        .attr("fill", "none")
-                        .attr("stroke", "white")
-                        .attr("stroke-width", 3)
-                    return ret;
-                }
-                );
-            link = link
-                .data(links, d => [d.source, d.target])
-                .join("path");
-
-            simulation.nodes(nodes);
-            simulation.force("link").links(links);
-            simulation.on("tick", () => {
+        var
+            links = this.data.links.map(d => Object.create(d)),
+            nodes = this.data.nodes.map(d => Object.create(d)),
+            link, node;
+        const
+            height = 700,
+            width = 1200,
+            simulation = d3.forceSimulation(nodes)
+                .force("link", d3.forceLink(links).id(d => d.id).distance(50))
+                .force("charge", d3.forceManyBody().strength(-400))
+                .force("x", d3.forceX())
+                .force("y", d3.forceY()),
+            svg = d3.select(this.template.querySelector(".d3"))
+                .append('svg')
+                .attr("viewBox", [-width / 2, -height / 2, width, height])
+                .style("font", "12px sans-serif"),
+            color = d3.scaleOrdinal(types, d3.schemeCategory10),
+            ticked = () => {
                 link.attr("d", linkArc);
                 node.attr("transform", d => `translate(${d.x},${d.y})`);
-            });
-            simulation.alpha(1).restart();
+            },
 
-        }
+            update = () => {
+                // Make a shallow copy to protect against mutation, while
+                // recycling old nodes to preserve position and velocity.
+                const old = new Map(node.data().map(d => [d.id, d]));
+                nodes = this.data.nodes.map(d => Object.assign(old.get(d.id) || {}, d));
+                links = this.data.links.map(d => Object.assign({}, d));
 
+                node = node
+                    .data(nodes, d => d.id)
+                    .join(enter => {
+                        var ret = enter.append("g")
+                            .call(drag(simulation))
+                            .on('mouseover', nodeMouseOver)
+                            .on('mouseout', nodeMouseOut)
+                            .on('click', nodeClicked);
+                        ret.append("circle")
+                            .attr("stroke", "white")
+                            .attr("stroke-width", 1.5)
+                            .attr("r", 4);
+                        ret.append("text")
+                            .attr("x", 8)
+                            .attr("y", "0.31em")
+                            .text(d => d.name)
+                            .clone(true).lower()
+                            .attr("fill", "none")
+                            .attr("stroke", "white")
+                            .attr("stroke-width", 3)
+                        return ret;
+                    });
+                link = link
+                    .data(links, d => [d.source, d.target])
+                    .join("path");
 
+                simulation.nodes(nodes);
+                simulation.force("link").links(links);
+                simulation.on("tick", ticked);
+                simulation.alpha(1).restart();
 
-        const linkArc = d => {
-            const r = Math.hypot(d.target.x - d.source.x, d.target.y - d.source.y);
-            return `
+            },
+
+            linkArc = d => {
+                const r = Math.hypot(d.target.x - d.source.x, d.target.y - d.source.y);
+                return `
               M${d.source.x},${d.source.y}
               L${d.target.x},${d.target.y}
             `;
-            /* use this if you like curvy lines
-            return `
-              M${d.source.x},${d.source.y}
-              A${r},${r} 0 0,1 ${d.target.x},${d.target.y}
-            `;*/
+                /* use this if you like curvy lines
+                return `
+                  M${d.source.x},${d.source.y}
+                  A${r},${r} 0 0,1 ${d.target.x},${d.target.y}
+                `;*/
+            },
 
-        }
+            drag = simulation => {
+                const
+                    dragstarted = (event, d) => {
+                        if (!event.active) simulation.alphaTarget(0.3).restart();
+                        d.fx = d.x;
+                        d.fy = d.y;
+                    },
+                    dragged = (event, d) => {
+                        d.fx = event.x;
+                        d.fy = event.y;
+                    },
+                    dragended = (event, d) => {
+                        if (!event.active) simulation.alphaTarget(0);
+                        d.fx = null;
+                        d.fy = null;
+                    }
 
-        var drag = simulation => {
-
-            function dragstarted(event, d) {
-                if (!event.active) simulation.alphaTarget(0.3).restart();
-                d.fx = d.x;
-                d.fy = d.y;
-            }
-
-            function dragged(event, d) {
-                d.fx = event.x;
-                d.fy = event.y;
-            }
-
-            function dragended(event, d) {
-                if (!event.active) simulation.alphaTarget(0);
-                d.fx = null;
-                d.fy = null;
-            }
-
-            return d3.drag()
-                .on("start", dragstarted)
-                .on("drag", dragged)
-                .on("end", dragended);
-        }
+                return d3.drag()
+                    .on("start", dragstarted)
+                    .on("drag", dragged)
+                    .on("end", dragended);
+            },
 
 
-        const nodeMouseOver = d => {
-            // Highlight the nodes: every node is green except of him
-            console.log("mouse over node", d);
-            //node.style('fill', "#B8B8B8")
-            d.target.setAttribute("style", "fill:#FF0000");
-            //d3.select(this).style('fill', '#69b3b2')
-            // Highlight the connections
-            link
-                .style('stroke', function (link_d) {
-                    return link_d.source === d.target.__data__ || link_d.target === d.target.__data__ ? '#000000' : color(link_d.type);
-                })
-            /*.style('stroke-width', function (link_d) {
-                return link_d.source === d.target.__data__ || link_d.target === d.target.__data__ ? 2 : 1.5;
-            })*/
-        };
-        const nodeMouseOut = d => {
-
-            d.target.setAttribute("style", "fill:#000000");
-            link
-                .style('stroke', dlink => color(dlink.type))
-            //.style('stroke-width', '1.5')
-        }
-
-        const nodeClicked = d => {
-
-            if (d.target.__data__.type === 'Contact') {
-                getGraphByContactId({
-                    contactId: d.target.__data__.id
-                })
-                    .then(result => {
-                        console.log('NODE CLICKED');
-                        console.log('this.data', this.data);
-                        console.log('result', result);
-                        const
-                            mergedLinks = this.data.links.concat(result.links),
-                            linkKeys = ['source', 'target'],
-                            uniqueLinks = mergedLinks.filter(
-                                (s => o =>
-                                    (k => !s.has(k) && s.add(k))
-                                        (linkKeys.map(k => o[k]).join('|'))
-                                )
-                                    (new Set)
-                            );
-                        const
-                            mergedNodes = this.data.nodes.concat(result.nodes),
-                            nodeKeys = ['id'],
-                            uniqueNodes = mergedNodes.filter(
-                                (s => o =>
-                                    (k => !s.has(k) && s.add(k))
-                                        (nodeKeys.map(k => o[k]).join('|'))
-                                )
-                                    (new Set)
-                            );
-
-
-                        this.data.links = [...uniqueLinks];
-                        this.data.nodes = [...uniqueNodes];
-                        update()
+            nodeMouseOver = d => {
+                console.log("mouse over node", d);
+                //node.style('fill', "#B8B8B8")
+                d.target.setAttribute("style", "fill:#FF0000");
+                //d3.select(this).style('fill', '#69b3b2')
+                // Highlight the connections
+                link
+                    .style('stroke', function (link_d) {
+                        return link_d.source === d.target.__data__ || link_d.target === d.target.__data__ ? '#000000' : color(link_d.type);
                     })
-                    .catch(error => {
-                        console.error(error)
-                    })
+                /*.style('stroke-width', function (link_d) {
+                    return link_d.source === d.target.__data__ || link_d.target === d.target.__data__ ? 2 : 1.5;
+                })*/
+            },
+            nodeMouseOut = d => {
+                d.target.setAttribute("style", "fill:#000000");
+                link.style('stroke', dlink => color(dlink.type))
+                //.style('stroke-width', '1.5')
+            },
+
+            dedupAndUpdateGraph = result => {
+                const
+                    mergedLinks = this.data.links.concat(result.links),
+                    linkKeys = ['source', 'target'],
+                    uniqueLinks = mergedLinks.filter(
+                        (s => o =>
+                            (k => !s.has(k) && s.add(k))
+                                (linkKeys.map(k => o[k]).join('|'))
+                        )
+                            (new Set)
+                    ),
+                    mergedNodes = this.data.nodes.concat(result.nodes),
+                    nodeKeys = ['id'],
+                    uniqueNodes = mergedNodes.filter(
+                        (s => o =>
+                            (k => !s.has(k) && s.add(k))
+                                (nodeKeys.map(k => o[k]).join('|'))
+                        )
+                            (new Set)
+                    );
+                this.data.links = [...uniqueLinks];
+                this.data.nodes = [...uniqueNodes];
+                update()
+            },
+
+            nodeClicked = d => {
+                if (d.target.__data__.type === 'Contact') {
+                    getGraphByContactId({ contactId: d.target.__data__.id })
+                        .then(result => dedupAndUpdateGraph(result))
+                        .catch(error => console.error(error))
+                }
+                if (d.target.__data__.type === 'Encounter') {
+                    getGraphByEncounterId({ encounterId: d.target.__data__.id })
+                        .then(result => dedupAndUpdateGraph(result))
+                        .catch(error => console.error(error))
+                }
             }
 
-            if (d.target.__data__.type === 'Encounter') {
-                getGraphByEncounterId({
-                    encounterId: d.target.__data__.id
-                })
-                    .then(result => {
-                        console.log('NODE CLICKED');
-                        console.log('this.data', this.data);
-                        console.log('result', result);
-                        const
-                            mergedLinks = this.data.links.concat(result.links),
-                            linkKeys = ['source', 'target'],
-                            uniqueLinks = mergedLinks.filter(
-                                (s => o =>
-                                    (k => !s.has(k) && s.add(k))
-                                        (linkKeys.map(k => o[k]).join('|'))
-                                )
-                                    (new Set)
-                            );
-                        const
-                            mergedNodes = this.data.nodes.concat(result.nodes),
-                            nodeKeys = ['id'],
-                            uniqueNodes = mergedNodes.filter(
-                                (s => o =>
-                                    (k => !s.has(k) && s.add(k))
-                                        (nodeKeys.map(k => o[k]).join('|'))
-                                )
-                                    (new Set)
-                            );
-
-
-                        this.data.links = [...uniqueLinks];
-                        this.data.nodes = [...uniqueNodes];
-                        update();
-                    })
-                    .catch(error => {
-                        console.error(error)
-                    })
-            }
-        }
-
-        // Per-type markers, as they don't inherit styles.
+        //Initialize the svg drawing
         svg.append("defs").selectAll("marker")
             .data(types)
             .join("marker")
@@ -294,7 +235,7 @@ export default class ContactTracingGraph extends LightningElement {
             .attr("fill", color)
             .attr("d", "M0,-5L10,0L0,5");
 
-        var link = svg.append("g")
+        link = svg.append("g")
             .attr("fill", "none")
             .attr("stroke-width", 1.5)
             .selectAll("path")
@@ -303,7 +244,7 @@ export default class ContactTracingGraph extends LightningElement {
             .attr("stroke", d => color(d.type))
             .attr("marker-end", d => `url(${new URL(`#arrow-${d.type}`, location)})`);
 
-        var node = svg.append("g")
+        node = svg.append("g")
             .attr("fill", "currentColor")
             .attr("stroke-linecap", "round")
             .attr("stroke-linejoin", "round")
@@ -329,13 +270,7 @@ export default class ContactTracingGraph extends LightningElement {
             .attr("stroke", "white")
             .attr("stroke-width", 3);
 
-        simulation.on("tick", () => {
-            link.attr("d", linkArc);
-            node.attr("transform", d => `translate(${d.x},${d.y})`);
-        });
-
-        //invalidation.then(() => simulation.stop());
-
+        simulation.on("tick", ticked);
     }
 
 
